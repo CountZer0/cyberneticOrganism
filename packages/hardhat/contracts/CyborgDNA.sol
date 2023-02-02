af442aecbe04e79b04c80d942027950dfa848f7b
Jason Parks <admin@continuityai.com>
2023-02-01 17:34:46 -0800

    Cyborg contract renamed

--- packages/hardhat/contracts/CyborgDNA.sol
+++ packages/hardhat/contracts/CyborgDNA.sol
@@ -7,7 +7,7 @@ import "./DiceUtilities.sol";
 import "./CyborgTables.sol";
 import "./Base64.sol";
 
-contract Cyborg is ERC721URIStorage{
+contract CyborgDNA is ERC721URIStorage{
 
     uint256 public tokenCounter;
 
@@ -22,8 +22,8 @@ contract Cyborg is ERC721URIStorage{
 
         int8 hitPoints;
 
-//        string externalUrl;
-//        string ipfs;
+        //        string externalUrl;
+        //        string ipfs;
         string ipfsURLs;
     }
 
@@ -41,8 +41,8 @@ contract Cyborg is ERC721URIStorage{
         int8 toughness,
         int8 knowledge,
         int8 hitPoints,
-//        string memory externalUrl,
-//        string memory ipfs
+    //        string memory externalUrl,
+    //        string memory ipfs
         string memory ipfsURLs
     ) public returns (uint256) {
         uint256 newTokenId = tokenCounter;
@@ -56,8 +56,8 @@ contract Cyborg is ERC721URIStorage{
                 toughness,
                 knowledge,
                 hitPoints,
-//                externalUrl,
-//                ipfs
+            //                externalUrl,
+            //                ipfs
                 ipfsURLs
             )
         );
@@ -80,8 +80,8 @@ contract Cyborg is ERC721URIStorage{
         int, // toughness
         int, // knowledge
         int, // hitPoints
-//        string memory, // externalUrl
-//        string memory // ipfs
+    //        string memory, // externalUrl
+    //        string memory // ipfs
         string memory // ipfsURLs
     )
     {
@@ -93,8 +93,8 @@ contract Cyborg is ERC721URIStorage{
         avatars[tokenId].toughness,
         avatars[tokenId].knowledge,
         avatars[tokenId].hitPoints,
-//        avatars[tokenId].externalUrl,
-//        avatars[tokenId].ipfs
+        //        avatars[tokenId].externalUrl,
+        //        avatars[tokenId].ipfs
         avatars[tokenId].ipfsURLs
         );
     }
@@ -120,23 +120,23 @@ contract Cyborg is ERC721URIStorage{
         parts[7] = Strings.toString(uint256(uint8(avatars[id].hitPoints)));
 
         // roll attribute data
-//        uint8[] memory oneToFiftyRolls = DiceUtilities.dieRollsMultiple(50, 1, 3);
-//        uint8[3] memory oneToFiftyRolls = [1,2,3];
+        //        uint8[] memory oneToFiftyRolls = DiceUtilities.dieRollsMultiple(50, 1, 3);
+        uint8[3] memory oneToFiftyRolls = [1,2,3];
 
-//        // TODO: AvatarTables
-//        bytes memory style = CyborgTables.getStyle(oneToFiftyRolls[0]-1);
-//        bytes memory style = getStyle(oneToFiftyRolls[0]-1);
-//        bytes memory feature = CyborgTables.getFeature(oneToFiftyRolls[1]-1);
-//        bytes memory feature = getFeature(oneToFiftyRolls[1]-1);
-//        string memory feature = getFeature(oneToFiftyRolls[1]-1);
-//        bytes memory obsession = CyborgTables.getObsession(oneToFiftyRolls[2]-1);
-//        bytes memory obsession = getObsession(oneToFiftyRolls[2]-1);
-//        string memory obsession = getObsession(oneToFiftyRolls[2]-1);
+        //        // TODO: AvatarTables
+        //        bytes memory style = CyborgTables.getStyle(oneToFiftyRolls[0]-1);
+        //        bytes memory style = getStyle(oneToFiftyRolls[0]-1);
+        //        bytes memory feature = CyborgTables.getFeature(oneToFiftyRolls[1]-1);
+        //        bytes memory feature = getFeature(oneToFiftyRolls[1]-1);
+        //        string memory feature = getFeature(oneToFiftyRolls[1]-1);
+        //        bytes memory obsession = CyborgTables.getObsession(oneToFiftyRolls[2]-1);
+        //        bytes memory obsession = getObsession(oneToFiftyRolls[2]-1);
+        //        string memory obsession = getObsession(oneToFiftyRolls[2]-1);
 
 
         string memory attributes = string(
             abi.encodePacked(
-//                '{ "trait_type": "Base", "value": "Cy80RG"}',
+            //                '{ "trait_type": "Base", "value": "Cy80RG"}',
                 '{"trait_type": "Strength", "value": ', parts[2], '}',
                 ', {"trait_type": "Agility", "value": ', parts[3], '}',
                 ', {"trait_type": "Presence", "value": ', parts[4], '}',
@@ -149,17 +149,17 @@ contract Cyborg is ERC721URIStorage{
             abi.encodePacked(
                 attributes,
                 ', {"display_type": "number", "trait_type": "Generation", "value": "1.0"}',
-//                ', {"trait_type": "Style", "value": "', style, '"}',
-//                ', {"trait_type": "Feature", "value": "', feature, '"}',
-//                ', {"trait_type": "Obsession", "value": "', obsession, '"}',
+            //                ', {"trait_type": "Style", "value": "', style, '"}',
+            //                ', {"trait_type": "Feature", "value": "', feature, '"}',
+            //                ', {"trait_type": "Obsession", "value": "', obsession, '"}',
                 ', {"trait_type": "HitPoints", "value": ', parts[7],'}'
             )
         );
 
-//        bytes memory image = "ipfs://bafybeibdbvbg7vzikrbdrvhrcyycw3es3rzdrjmeaubyiykxupmj6qpiq4";
+        //        bytes memory image = "ipfs://bafybeibdbvbg7vzikrbdrvhrcyycw3es3rzdrjmeaubyiykxupmj6qpiq4";
 
-//        bytes memory ipfsImage = avatars[id].ipfsURLs;
-//        bytes memory externalUrlImage = avatars[id].ipfsURLs;
+        //        bytes memory ipfsImage = avatars[id].ipfsURLs;
+        //        bytes memory externalUrlImage = avatars[id].ipfsURLs;
 
         return string(
             abi.encodePacked(
@@ -170,12 +170,12 @@ contract Cyborg is ERC721URIStorage{
                             '{"name":"',
                             parts[1],
                             '", "image":"',
-//                            image,
-//                            avatars[id].ipfs,
-                                avatars[id].ipfsURLs,
+                        //                            image,
+                        //                            avatars[id].ipfs,
+                            avatars[id].ipfsURLs,
                             '", "external_url":"',
-//                            avatars[id].externalUrl,
-                                avatars[id].ipfsURLs,
+                        //                            avatars[id].externalUrl,
+                            avatars[id].ipfsURLs,
                             '", "description": "I am souldbound to 0x', Strings.toHexString(walletAddress), '."',
                             ', "attributes": [',
                             attributes,
