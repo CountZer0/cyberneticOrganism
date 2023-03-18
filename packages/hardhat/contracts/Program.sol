6b508f501290655da8af299304ea082737314072
Jason Parks <admin@continuityai.com>
2023-03-18 01:40:03 -0700

    Program.sol draft

--- packages/hardhat/contracts/Program.sol
+++ packages/hardhat/contracts/Program.sol
@@ -3,41 +3,41 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./CyberneticOrganism.sol";
 
-contract Weapon is ERC721URIStorage{
+contract Program is ERC721URIStorage {
 
     uint256 public tokenCounter;
     address cyberneticOrganismAddress;
 
-    struct WeaponStats {
+    struct ProgramStats {
         string name;
-        string class;
+        string description;
 
-        int8 level;
+        int8 damage;
 
         string ipfsURLs;
     }
 
-    WeaponStats[] public weaponStats;
+    ProgramStats[] public programStats;
 
-    constructor(address addr) ERC721 ("Weapon", "WPEN") {
+    constructor(address addr) ERC721 ("Program", "PRGM") {
         tokenCounter = 0;
         cyberneticOrganismAddress = addr;
     }
 
-    function createWeapon(
+    function createProgram(
         string memory name,
-        string memory class,
-        int8 level,
+        string memory description,
+        int8 damage,
         string memory ipfsURLs
     ) public returns (uint256) {
         uint256 newTokenId = tokenCounter;
 
-        weaponStats.push(
-            WeaponStats(
+        programStats.push(
+            ProgramStats(
                 name,
-                class,
-                level,
-                ipfsURLs
+                    description,
+                    damage,
+                    ipfsURLs
             )
         );
 
@@ -48,54 +48,54 @@ contract Weapon is ERC721URIStorage{
         return newTokenId;
     }
 
-    function doDamage(uint256 tokenId, int8 damage) virtual public{
+    function doDamage(uint256 tokenId, int8 damage) public{
         CyberneticOrganism co = CyberneticOrganism(cyberneticOrganismAddress);
         co.adjustHitPoints(tokenId, damage);
         // advance level
-        weaponStats[tokenId].level += 1;
+        programStats[tokenId].damage +=1;
         // update uri
         _setTokenURI(tokenId, _buildTokenURI(tokenId, msg.sender));
     }
 
-    function getCyberneticOrganismAddress() virtual public view returns (address) {
+    function getCyberneticOrganismAddress() public view returns (address) {
         return cyberneticOrganismAddress;
     }
 
-    function getWeaponStats(uint8 tokenId) public view returns (
+    function getProgramStats(uint8 tokenId) public view returns (
         string memory, // name
-        string memory, // class
-        int8, // level
+        string memory, // description
+        int8, // damage
         string memory // ipfsURLs
     )
     {
         return (
-        weaponStats[tokenId].name,
-        weaponStats[tokenId].class,
-        weaponStats[tokenId].level,
-        weaponStats[tokenId].ipfsURLs
+        programStats[tokenId].name,
+        programStats[tokenId].description,
+        programStats[tokenId].damage,
+        programStats[tokenId].ipfsURLs
         );
     }
 
-    function _buildTokenURI(uint256 id, address walletAddress) nternal view returns (string memory) {
+    function _buildTokenURI(uint256 id, address walletAddress) internal view returns (string memory) {
 
         // We create the an array of string with max length 17
         string[3] memory parts;
-        parts[0] = weaponStats[id].name;
-        parts[1] = weaponStats[id].class;
-        parts[2] = Strings.toString(uint256(uint8(weaponStats[id].level)));
+        parts[0] = programStats[id].name;
+        parts[1] = programStats[id].description;
+        parts[2] = Strings.toString(uint256(uint8(programStats[id].damage)));
 
 
         // roll attribute data
 //        uint8[] memory oneToFiftyRolls = DiceUtilities.dieRollsMultiple(50, 1, 3);
 
-        // TODO: WeaponTables
+        // TODO: ProgramTables
 //        bytes memory style = CyborgTables.getStyle(oneToFiftyRolls[0]-1);
 
         string memory attributes = string(
             abi.encodePacked(
                 '{"trait_type": "Name", "value": ', parts[0], '}',
-                ', {"trait_type": "Class", "value": ', parts[1], '}',
-                ', {"trait_type": "Level", "value": ', parts[2], '}'
+                ', {"trait_type": "Description", "value": ', parts[1], '}',
+                ', {"trait_type": "Damage", "value": ', parts[2], '}'
             )
         );
 
@@ -108,9 +108,9 @@ contract Weapon is ERC721URIStorage{
                             '{"name":"',
                                 parts[0],
                             '", "image":"',
-                                weaponStats[id].ipfsURLs,
+                                programStats[id].ipfsURLs,
                             '", "external_url":"',
-                                weaponStats[id].ipfsURLs,
+                                programStats[id].ipfsURLs,
                             '", "description": "I am souldbound to 0x', Strings.toHexString(walletAddress), '."',
                             ', "attributes": [',
                                 attributes,
